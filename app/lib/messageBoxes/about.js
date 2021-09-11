19115e045d4e6e88b8efb72d61375d85052656c5
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2021-09-11 17:42:07 +0200

    feat(issue 23): add an "About..." dialog to display information and links related to the application

---
+++ app/lib/messageBoxes/about.js
@@ -0,0 +1,28 @@
+const { default: openAboutWindow } = require( 'about-window' )
+const path = require( "path" )
+
+function showAboutWindow(parentWindow) {
+  let aboutWin = openAboutWindow( {
+    icon_path: path.join( __dirname, '../../../build/icon.png' ),
+    win_options: {
+      parent: parentWindow,
+      modal: true,
+      resizable: false,
+      skipTaskbar: true,
+      title: "About ...",
+      minimizable: false,
+      maximizable: false
+    },
+    package_json_dir: path.join( __dirname, '../../../' )
+  } );
+  
+  aboutWin.webContents.on( 'before-input-event', ( event, input ) => {
+    if ( input.key == 'Escape' ) {
+      aboutWin.close();
+      event.preventDefault();
+    }
+  } )
+  
+}
+
+module.exports.showAboutWindow = showAboutWindow
